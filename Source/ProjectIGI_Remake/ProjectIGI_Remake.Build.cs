b4488bf9bc6c255beedd4d384d0ff3cd1014bc63
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-21 10:50:04 +0330

    Add Source/ProjectIGI_Remake/ProjectIGI_Remake.Build.cs

---
+++ Source/ProjectIGI_Remake/ProjectIGI_Remake.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+
+public class ProjectIGI_Remake : ModuleRules
+{
+	public ProjectIGI_Remake(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"GameplayTags",
+			"ALS"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+	}
+}
