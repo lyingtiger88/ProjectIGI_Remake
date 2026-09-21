ac5417aedc429c5ab2c79e67b92f101bd6225106
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-21 10:50:10 +0330

    Add Source/ProjectIGI_Remake/BP_IGI_Player.h

---
+++ Source/ProjectIGI_Remake/BP_IGI_Player.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IGIPlayerCharacter.h"
+#include "BP_IGI_Player.generated.h"
+
+UCLASS()
+class PROJECTIGI_REMAKE_API ABP_IGI_Player : public AIGIPlayerCharacter
+{
+	GENERATED_BODY()
+};
