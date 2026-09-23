41d2a341c54165ee76bd4992fbf4eca2791f8639
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-24 02:23:09 +0330

    Add med kit world pickup

---
+++ Source/ProjectIGI_Remake/Pickups/IGIMedKitPickupActor.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "IGIMedKitPickupActor.generated.h"
+
+class UPrimitiveComponent;
+class USphereComponent;
+class UStaticMeshComponent;
+
+UCLASS(Blueprintable, BlueprintType, meta = (DisplayName = "IGI Med Kit Pickup"))
+class PROJECTIGI_REMAKE_API AIGIMedKitPickupActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AIGIMedKitPickupActor();
+
+    UFUNCTION(BlueprintCallable, Category = "IGI|Pickup")
+    bool TryPickupByActor(AActor* OtherActor);
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IGI|Pickup")
+    TObjectPtr<USphereComponent> PickupSphere;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IGI|Pickup")
+    TObjectPtr<UStaticMeshComponent> PickupMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Pickup", meta = (ClampMin = "1"))
+    int32 MedKitCount = 1;
+
+private:
+    UFUNCTION()
+    void HandlePickupOverlap(
+        UPrimitiveComponent* OverlappedComponent,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComponent,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult);
+};
