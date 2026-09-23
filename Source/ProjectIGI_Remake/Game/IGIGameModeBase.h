95552101adf5c5fd69e3c1e563a2d7908d4ba192
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 21:28:07 +0330

    Add native IGI game mode

---
+++ Source/ProjectIGI_Remake/Game/IGIGameModeBase.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IGIGameModeBase.generated.h"
+
+UCLASS()
+class PROJECTIGI_REMAKE_API AIGIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIGIGameModeBase();
+};
