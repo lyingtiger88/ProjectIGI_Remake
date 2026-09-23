26f765f0cebcdcb93167a5efa114092fec2a9dc6
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-24 02:03:48 +0330

    Drive flare gun behavior from weapon data

--- Source/ProjectIGI_Remake/Weapons/IGIFlareGunBase.h
+++ Source/ProjectIGI_Remake/Weapons/IGIFlareGunBase.h
@@ -4,10 +4,6 @@
 #include "Weapons/IGIFirearmBase.h"
 #include "IGIFlareGunBase.generated.h"
 
-class AIGIFlareProjectileActor;
-class UNiagaraSystem;
-class UStaticMesh;
-
 UCLASS(Blueprintable, BlueprintType, meta = (DisplayName = "IGI Flare Gun"))
 class PROJECTIGI_REMAKE_API AIGIFlareGunBase : public AIGIFirearmBase
 {
@@ -17,23 +13,4 @@ public:
     AIGIFlareGunBase();
 
     virtual bool FireHitscan(AController* InstigatorController) override;
-
-protected:
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare")
-    EIGIFlarePurpose FlarePurpose = EIGIFlarePurpose::Illumination;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare")
-    TSubclassOf<AIGIFlareProjectileActor> FlareProjectileClass;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare")
-    TSoftObjectPtr<UStaticMesh> FlareProjectileMesh;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare")
-    TSoftObjectPtr<UNiagaraSystem> FlareTrailEffect;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare", meta = (ClampMin = "100.0", ForceUnits = "cm/s"))
-    float FlareLaunchSpeed = 1800.0f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IGI|Flare", meta = (ClampMin = "1.0", ForceUnits = "s"))
-    float FlareLifeSeconds = 22.0f;
 };
