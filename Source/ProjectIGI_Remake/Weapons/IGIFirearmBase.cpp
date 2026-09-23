d125e3fcd90532aff6d5bb1dd9f08e867a0ff880
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 22:53:49 +0330

    Implement camera-directed hitscan fire and damage

--- Source/ProjectIGI_Remake/Weapons/IGIFirearmBase.cpp
+++ Source/ProjectIGI_Remake/Weapons/IGIFirearmBase.cpp
@@ -1,6 +1,10 @@
 #include "Weapons/IGIFirearmBase.h"
 
+#include "Engine/World.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/Pawn.h"
 #include "Inventory/IGIInventoryComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "Weapons/IGIWeaponAttachmentComponent.h"
 #include "Weapons/IGIWeaponDataAsset.h"
 
@@ -91,6 +95,58 @@ bool AIGIFirearmBase::NotifyShotFired()
     }
 
     ReportGunshotNoise();
+    return true;
+}
+
+bool AIGIFirearmBase::FireHitscan(AController* InstigatorController)
+{
+    bLastShotHit = false;
+    LastShotImpactLocation = FVector::ZeroVector;
+
+    if (!IsValid(WeaponData) || !IsValid(InstigatorController) || !NotifyShotFired())
+    {
+        return false;
+    }
+
+    FVector ViewLocation = GetActorLocation();
+    FRotator ViewRotation = GetActorRotation();
+    InstigatorController->GetPlayerViewPoint(ViewLocation, ViewRotation);
+
+    const float SpreadRadians = FMath::DegreesToRadians(FMath::Max(0.0f, GetEffectiveSpread()));
+    const FVector ShotDirection = SpreadRadians > KINDA_SMALL_NUMBER
+        ? FMath::VRandCone(ViewRotation.Vector(), SpreadRadians)
+        : ViewRotation.Vector();
+
+    const FVector TraceEnd = ViewLocation + ShotDirection * FMath::Max(1.0f, WeaponData->EffectiveRangeCm);
+
+    FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(IGIWeaponHitscan), true, GetOwner());
+    QueryParams.AddIgnoredActor(this);
+
+    FHitResult Hit;
+    UWorld* World = GetWorld();
+
+    if (!IsValid(World) || !World->LineTraceSingleByChannel(Hit, ViewLocation, TraceEnd, WeaponTraceChannel, QueryParams))
+    {
+        LastShotImpactLocation = TraceEnd;
+        return true;
+    }
+
+    bLastShotHit = true;
+    LastShotImpactLocation = Hit.ImpactPoint;
+
+    AActor* HitActor = Hit.GetActor();
+    if (IsValid(HitActor))
+    {
+        UGameplayStatics::ApplyPointDamage(
+            HitActor,
+            WeaponData->BaseDamage,
+            ShotDirection,
+            Hit,
+            InstigatorController,
+            GetAcousticInstigator(),
+            nullptr);
+    }
+
     return true;
 }
 
