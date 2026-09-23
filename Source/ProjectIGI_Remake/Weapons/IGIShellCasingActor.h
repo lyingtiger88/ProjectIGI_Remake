ea0dc9ac34918516936a095e0063a094b1b1ec47
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-24 00:23:25 +0330

    Add IGIShellCasingActor.h

---
+++ Source/ProjectIGI_Remake/Weapons/IGIShellCasingActor.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Environment/IGIWeatherTypes.h"
+#include "IGIShellCasingActor.generated.h"
+
+class UStaticMesh;
+class UStaticMeshComponent;
+
+UCLASS(Blueprintable, BlueprintType, meta = (DisplayName = "IGI Shell Casing"))
+class PROJECTIGI_REMAKE_API AIGIShellCasingActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    AIGIShellCasingActor();
+
+    virtual void Tick(float DeltaSeconds) override;
+
+    UFUNCTION(BlueprintCallable, Category = "IGI|Weapon|Casing")
+    void InitializeCasing(
+        UStaticMesh* InCasingMesh,
+        const FVector& InitialVelocity,
+        const FVector& InitialAngularVelocityDegrees,
+        float LifeSeconds);
+
+    UFUNCTION(BlueprintPure, Category = "IGI|Weapon|Casing")
+    EIGIWeatherType GetSpawnWeatherType() const { return SpawnWeatherState.WeatherType; }
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IGI|Weapon|Casing")
+    TObjectPtr<UStaticMeshComponent> CasingMesh;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "IGI|Weapon|Casing")
+    float WindForceScale = 0.018f;
+
+private:
+    FIGIWeatherState SpawnWeatherState;
+    float WeatherWindResponse = 0.0f;
+
+    void ApplyWeatherProfile();
+};
