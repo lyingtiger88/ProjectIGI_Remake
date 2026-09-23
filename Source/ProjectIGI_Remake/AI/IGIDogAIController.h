c7fda4163a9630ae507e2bf229e0b90ba40d7e2b
BDFR <66498080+lyingtiger88@users.noreply.github.com>
2026-09-23 13:15:49 +0330

    Add BDFR canine AI controller

---
+++ Source/ProjectIGI_Remake/AI/IGIDogAIController.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behavior/BDFRCanineAIController.h"
+#include "IGIDogAIController.generated.h"
+
+UCLASS(Blueprintable)
+class PROJECTIGI_REMAKE_API AIGIDogAIController : public ABDFRCanineAIController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool BDFR_ShouldProcessPerceivedActor_Implementation(AActor* SourceActor) const override;
+};
